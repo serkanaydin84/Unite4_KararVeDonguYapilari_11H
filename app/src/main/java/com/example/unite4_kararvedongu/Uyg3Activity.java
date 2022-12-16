68ef90e30dfce6da8f1b3f899c48a63b147788af
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-16 10:38:21 +0300

    uygulama 3 login işlemi

--- app/src/main/java/com/example/unite4_kararvedongu/Uyg3Activity.java
+++ app/src/main/java/com/example/unite4_kararvedongu/Uyg3Activity.java
@@ -1,14 +1,34 @@
 package com.example.unite4_kararvedongu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Uyg3Activity extends AppCompatActivity {
 
+    EditText txtUserName, txtPassword;
+
+    final String userName = "BomberMan";
+    final String password = "123";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg3_activity);
+
+        txtUserName = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPass);
+    }
+
+    public void btnLogin(View view) {
+        if (userName.equals(txtUserName.getText().toString()) && password.equals(txtPassword.getText().toString())) {
+            Toast.makeText(this, "Hadi yine iyisi giriş yapabildin, Artist", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Kullanıcı Adın veya şifren hatalı, ne işin var buralarda", Toast.LENGTH_LONG).show();
+        }
     }
 }
